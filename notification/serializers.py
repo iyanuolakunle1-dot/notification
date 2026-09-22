fffef44a957e2a13bdf2072a8e296e9e855b3b58
iyanu <iyanuolakunle1@gmail.com>
2026-09-22 14:31:10 +0100

    settings8

--- notification/serializers.py
+++ notification/serializers.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializers(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model=User
         fields = ["id", "username", "email", "password"]
